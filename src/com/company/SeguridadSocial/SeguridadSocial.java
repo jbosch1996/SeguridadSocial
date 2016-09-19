08376d778c181bca72c664b20cd141a8ca9ed4a9
seyker1996 <boschruzjoan@gmail.com>
2016-09-19 20:21:39 +0200

    Persona

---
+++ src/com/company/SeguridadSocial/SeguridadSocial.java
@@ -0,0 +1,45 @@
+package com.company.SeguridadSocial;
+
+    import java.util.ArrayList;
+    import java.util.List;
+
+    public class SeguridadSocial {
+        private List<Persona> personasList;
+        public SeguridadSocial() {
+            personasList = new ArrayList<>();
+        }
+
+        // Debes comprobar que no se introduzcan dos personas con el mismo DNI/Número Seguridad Social
+        public void altaPersona(Persona persona) {
+            personasList.add(persona);
+        }
+
+        public void bajaPersona(String dni) {
+            personasList.remove(dni);
+        }
+
+        public Persona obtenerPersonaPorDNI(String dni) {
+            if()
+        }
+
+        public Persona obtenerPersonaPorNumSS(String numSS) {
+        }
+
+        public List<Persona> obtenerPersonasRangoSalarial(double min, double max){
+        }
+
+        public List<Persona> obtenerPersonasMayoresQue(int edad){
+        }
+
+        public List<Persona> obtenerTodas(){
+        }
+
+        @Override
+        public String toString() {
+            return "SeguridadSocial{" +
+                    "personasList=" + personasList +
+                    '}';
+        }
+    }
+
+
