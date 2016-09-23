f2aa917ce6499696aa9f4fe3ea223f711d730d7e
seyker1996 <boschruzjoan@gmail.com>
2016-09-23 18:01:18 +0200

    Persona

--- src/com/company/SeguridadSocial/SeguridadSocialHashMap.java
+++ src/com/company/SeguridadSocial/SeguridadSocialHashMap.java
@@ -1,6 +1,8 @@
 package com.company.SeguridadSocial;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -8,16 +10,17 @@ import java.util.Map;
  */
 public class SeguridadSocialHashMap {
 
-    private Map<String,Persona> personaMapDni = new HashMap<>();
-    private Map<String,Persona> personaMapSs = new HashMap<>();
+    private Map<String, Persona> personaMapDni = new HashMap<>();
+    private Map<String, Persona> personaMapSs = new HashMap<>();
 
     public void altaPersona(Persona persona) {
 
-        if(!personaMapDni.containsKey(persona.getDni()) && !personaMapSs.containsKey(persona.getNum_seguridad())){
-            personaMapDni.put(persona.getDni(),persona);
-            personaMapSs.put(persona.getNum_seguridad(),persona);
+        if (!personaMapDni.containsKey(persona.getDni()) && !personaMapSs.containsKey(persona.getNum_seguridad())) {
+            personaMapDni.put(persona.getDni(), persona);
+            personaMapSs.put(persona.getNum_seguridad(), persona);
         }
     }
+
     public Persona obtenerPersonaPorDNI(String dni, String nombre) {
         //Para obtener las keys
         //personaMapDni.keySet();//Obtener todas las Keys
@@ -25,7 +28,24 @@ public class SeguridadSocialHashMap {
         //personaMapDni.remove(dni);
         return personaMapDni.get(dni);
     }
+
+    public void bajaPersona(String dni) {
+        personaMapDni.remove(dni);
     }
+
+    public Persona obtenerPersonaPorNumSS(String numSS) {
+        return personaMapSs.get(numSS);
+    }
+
+    public List<Persona> obtenerPersonasRangoSalarial(Persona persona, double min, double max) {
+        List<Persona> aux = new ArrayList<>();
+        if (persona.getSalario() >= min && persona.getSalario() <= max) {
+            aux.add(persona);
+
+        }
+        return aux;
+    }
+}
 
 
 
