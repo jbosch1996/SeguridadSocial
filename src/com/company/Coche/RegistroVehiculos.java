aa4206025236bc6449f6ee3ba4220dad25610270
seyker1996 <boschruzjoan@gmail.com>
2016-10-07 16:38:07 +0200

    Coche

--- src/com/company/Coche/RegistroVehiculos.java
+++ src/com/company/Coche/RegistroVehiculos.java
@@ -6,10 +6,18 @@ public class RegistroVehiculos {
     private Set<Coche> coches = new HashSet<>();
 
     public void registrarVehiculo(Coche coche) {
-
-
+        coches.add(coche);
     }
 
+    public Coche obtenerVehiculo(String matricula) {
+        for (Coche cocheList : coches) {
+            if (cocheList.getMatricula().equals(matricula)) {
+                return cocheList;
+            }
+        }
+
+        return null;
+    }
 
     public void eliminarVehiculo(String matricula) {
         Coche aux = null;
@@ -20,30 +28,43 @@ public class RegistroVehiculos {
             }
         }
         if (aux != null) {
-            cocheActual.remove(aux);
+            coches.remove(aux);
         }
     }
 
 
-    public Optional<Coche> obtenerVehiculoPrecioMax() {
+    public Coche obtenerVehiculoPrecioMax() {
 
         Coche max = null;
-
 
         for (Coche coche : coches) {
             if (max == null) {
                 max = coche;
-            }
-            else if(coche.getPrecio() > max){
+            } else if (coche.getPrecio() > max) {
                 max = coche;
             }
         }
-        return coche;
+        return max;
     }
 
     public List<Coche> obtenerVehiculosMarca(String marca) {
+        List<Coche> ListCoches = new ArrayList<>();
+        for (Coche coche : coches) {
+            if (coche.getMarca().equals(marca)) {
+                ListCoches.add(coche);
+            }
+        }
+
+        return null;
     }
 
+
     public List<Coche> obtenerTodos() {
+        List<Coche> ListCoches = new ArrayList<>();
+        for (Coche coche : coches) {
+            ListCoches.add(coche);
+        }
+        return ListCoches;
     }
+
 }
